f028250eb6a9823051c9dd6a218b049410ce5f76
Arcyvilk <blazemakerrr@gmail.com>
2017-11-11 20:37:12 +0100

    Added upgrades data

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,12 +31,14 @@
             this.components = new System.ComponentModel.Container();
             this.timerCount = new System.Windows.Forms.Timer(this.components);
             this.pictureBoxZombies = new System.Windows.Forms.PictureBox();
-            this.panelUpgrades = new System.Windows.Forms.Panel();
             this.pictureBoxButton = new System.Windows.Forms.PictureBox();
             this.dataBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.labelBrainsCount = new System.Windows.Forms.Label();
             this.labelZombieCount = new System.Windows.Forms.Label();
             this.pictureBoxBrains = new System.Windows.Forms.PictureBox();
+            this.labelZombiesPerSecond = new System.Windows.Forms.Label();
+            this.labelZombiesPerClick = new System.Windows.Forms.Label();
+            this.listBoxUpgrades = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxZombies)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButton)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).BeginInit();
@@ -46,49 +48,42 @@
             // timerCount
             // 
             this.timerCount.Interval = 1000;
+            this.timerCount.Tick += new System.EventHandler(this.timerCount_Tick);
             // 
             // pictureBoxZombies
             // 
             this.pictureBoxZombies.BackColor = System.Drawing.Color.Transparent;
             this.pictureBoxZombies.Image = global::ZombieSim.Properties.Resources.zombie;
-            this.pictureBoxZombies.Location = new System.Drawing.Point(96, 322);
+            this.pictureBoxZombies.Location = new System.Drawing.Point(96, 378);
             this.pictureBoxZombies.Name = "pictureBoxZombies";
             this.pictureBoxZombies.Size = new System.Drawing.Size(67, 70);
             this.pictureBoxZombies.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxZombies.TabIndex = 8;
             this.pictureBoxZombies.TabStop = false;
             // 
-            // panelUpgrades
-            // 
-            this.panelUpgrades.BackColor = System.Drawing.Color.Transparent;
-            this.panelUpgrades.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panelUpgrades.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelUpgrades.Location = new System.Drawing.Point(603, 0);
-            this.panelUpgrades.Name = "panelUpgrades";
-            this.panelUpgrades.Size = new System.Drawing.Size(341, 561);
-            this.panelUpgrades.TabIndex = 7;
-            // 
             // pictureBoxButton
             // 
             this.pictureBoxButton.BackColor = System.Drawing.Color.Transparent;
             this.pictureBoxButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBoxButton.Image = global::ZombieSim.Properties.Resources.buttonReady;
-            this.pictureBoxButton.Location = new System.Drawing.Point(167, 66);
+            this.pictureBoxButton.Location = new System.Drawing.Point(167, 27);
             this.pictureBoxButton.Name = "pictureBoxButton";
             this.pictureBoxButton.Size = new System.Drawing.Size(245, 240);
             this.pictureBoxButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxButton.TabIndex = 6;
             this.pictureBoxButton.TabStop = false;
             this.pictureBoxButton.Click += new System.EventHandler(this.pictureBoxButton_Click);
+            this.pictureBoxButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxButton_MouseDown);
+            this.pictureBoxButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxButton_MouseUp);
             // 
             // labelBrainsCount
             // 
             this.labelBrainsCount.BackColor = System.Drawing.Color.Transparent;
             this.labelBrainsCount.Font = new System.Drawing.Font("Northwood High", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelBrainsCount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.labelBrainsCount.Location = new System.Drawing.Point(379, 340);
+            this.labelBrainsCount.Location = new System.Drawing.Point(179, 469);
             this.labelBrainsCount.Name = "labelBrainsCount";
-            this.labelBrainsCount.Size = new System.Drawing.Size(115, 38);
+            this.labelBrainsCount.Size = new System.Drawing.Size(233, 38);
             this.labelBrainsCount.TabIndex = 11;
             this.labelBrainsCount.Text = "0";
             this.labelBrainsCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -98,9 +93,9 @@
             this.labelZombieCount.BackColor = System.Drawing.Color.Transparent;
             this.labelZombieCount.Font = new System.Drawing.Font("Northwood High", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelZombieCount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.labelZombieCount.Location = new System.Drawing.Point(179, 340);
+            this.labelZombieCount.Location = new System.Drawing.Point(179, 396);
             this.labelZombieCount.Name = "labelZombieCount";
-            this.labelZombieCount.Size = new System.Drawing.Size(111, 38);
+            this.labelZombieCount.Size = new System.Drawing.Size(233, 38);
             this.labelZombieCount.TabIndex = 10;
             this.labelZombieCount.Text = "0";
             this.labelZombieCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -109,29 +104,68 @@
             // 
             this.pictureBoxBrains.BackColor = System.Drawing.Color.Transparent;
             this.pictureBoxBrains.Image = global::ZombieSim.Properties.Resources.brain;
-            this.pictureBoxBrains.Location = new System.Drawing.Point(296, 322);
+            this.pictureBoxBrains.Location = new System.Drawing.Point(96, 454);
             this.pictureBoxBrains.Name = "pictureBoxBrains";
             this.pictureBoxBrains.Size = new System.Drawing.Size(67, 70);
             this.pictureBoxBrains.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxBrains.TabIndex = 9;
             this.pictureBoxBrains.TabStop = false;
             // 
+            // labelZombiesPerSecond
+            // 
+            this.labelZombiesPerSecond.BackColor = System.Drawing.Color.Transparent;
+            this.labelZombiesPerSecond.Font = new System.Drawing.Font("Northwood High", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelZombiesPerSecond.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.labelZombiesPerSecond.Location = new System.Drawing.Point(96, 279);
+            this.labelZombiesPerSecond.Name = "labelZombiesPerSecond";
+            this.labelZombiesPerSecond.Size = new System.Drawing.Size(390, 26);
+            this.labelZombiesPerSecond.TabIndex = 12;
+            this.labelZombiesPerSecond.Text = "0 zombies per second";
+            this.labelZombiesPerSecond.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelZombiesPerClick
+            // 
+            this.labelZombiesPerClick.BackColor = System.Drawing.Color.Transparent;
+            this.labelZombiesPerClick.Font = new System.Drawing.Font("Northwood High", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelZombiesPerClick.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.labelZombiesPerClick.Location = new System.Drawing.Point(96, 305);
+            this.labelZombiesPerClick.Name = "labelZombiesPerClick";
+            this.labelZombiesPerClick.Size = new System.Drawing.Size(390, 26);
+            this.labelZombiesPerClick.TabIndex = 13;
+            this.labelZombiesPerClick.Text = "1 zombies per click";
+            this.labelZombiesPerClick.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBoxUpgrades
+            // 
+            this.listBoxUpgrades.BackColor = System.Drawing.Color.Black;
+            this.listBoxUpgrades.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBoxUpgrades.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listBoxUpgrades.FormattingEnabled = true;
+            this.listBoxUpgrades.ItemHeight = 32;
+            this.listBoxUpgrades.Location = new System.Drawing.Point(527, 0);
+            this.listBoxUpgrades.Name = "listBoxUpgrades";
+            this.listBoxUpgrades.Size = new System.Drawing.Size(417, 561);
+            this.listBoxUpgrades.TabIndex = 14;
+            // 
             // FormMain
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = global::ZombieSim.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(944, 561);
+            this.Controls.Add(this.listBoxUpgrades);
+            this.Controls.Add(this.labelZombiesPerClick);
+            this.Controls.Add(this.labelZombiesPerSecond);
             this.Controls.Add(this.pictureBoxZombies);
-            this.Controls.Add(this.panelUpgrades);
             this.Controls.Add(this.pictureBoxButton);
             this.Controls.Add(this.labelBrainsCount);
             this.Controls.Add(this.labelZombieCount);
             this.Controls.Add(this.pictureBoxBrains);
+            this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Northwood High", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Zombie Apocalypse Simulator";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxZombies)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButton)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).EndInit();
@@ -144,12 +178,14 @@
 
         private System.Windows.Forms.Timer timerCount;
         private System.Windows.Forms.PictureBox pictureBoxZombies;
-        private System.Windows.Forms.Panel panelUpgrades;
         private System.Windows.Forms.PictureBox pictureBoxButton;
         private System.Windows.Forms.BindingSource dataBindingSource;
         private System.Windows.Forms.Label labelBrainsCount;
         private System.Windows.Forms.Label labelZombieCount;
         private System.Windows.Forms.PictureBox pictureBoxBrains;
+        private System.Windows.Forms.Label labelZombiesPerSecond;
+        private System.Windows.Forms.Label labelZombiesPerClick;
+        private System.Windows.Forms.ListBox listBoxUpgrades;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,11 +15,41 @@ namespace ZombieSim
         /// <summary>
         /// Class storing the vague data on all the game's objects.
         /// </summary>
-        public class GameData
+        public static class GameData
         {
-            public string Title { get; set; }
-            public Zombies Zombies { get; set; }
-            public Upgrades Upgrades { get; set; }
+            public static string Title { get; set; } = "Zombie Apocalypse Simulator";
+            public static string GameDescription { get; set; } = "Every time you press a button," +
+                "one person somewhere in the world gets turned into a zombie.\n" +
+                "Your goal is to turn the whole humanity into zombies. " +
+                "Using brains (1 zombie = 1 brain), you can unlock unique upgrades which speed up this process.\n\n" +
+                "Good luck!";
+            public static Zombies Zombies { get; set; } = new Zombies();
+            public static Upgrades Viruses { get; set; } = 
+                new Upgrades("Virus", "virus.png", "A simple virus to turn people into mindless carcasses.", 10, 0, 1, 0);
+            public static Upgrades InfectedMongrels { get; set; } = 
+                new Upgrades("Infected mongrel", "mongrel.png", "A homeless dog haunted by the disease. Warning, it bites!", 50, 0, 5, 0);
+            public static Upgrades Vaccines { get; set; } = 
+                new Upgrades("Vaccine causing zombizm", "vaccine.png", "Vaccine causing zombizm. You cannot say they didn't warn you.", 100, 0, 10, 0);
+            public static Upgrades ToxicWastes { get; set; } = 
+                new Upgrades("Toxic waste", "waste.png", "Toxic, radioactive wastes.", 400, 0, 20, 0);
+            public static Upgrades MadScientists { get; set; } = 
+                new Upgrades("Mad scientist", "scientist.png", "This nice scientist devoted his life to turn the whole humanity into zombies.", 1000, 0, 50, 0);
+            public static Upgrades Altars { get; set; } = 
+                new Upgrades("Altar for the Old Gods", "altar.png", "The Old Gods like altars and bloody sacrifices.", 2000, 0, 100, 0);
+            public static List<Upgrades> Upgrades = new List<Upgrades>();
+            public static void fillUpgradeList() {
+                GameData.Upgrades.Add(GameData.Viruses);
+                GameData.Upgrades.Add(GameData.InfectedMongrels);
+                GameData.Upgrades.Add(GameData.Vaccines);
+                GameData.Upgrades.Add(GameData.ToxicWastes);
+                GameData.Upgrades.Add(GameData.MadScientists);
+                GameData.Upgrades.Add(GameData.Altars);
+            }
+            static GameData(){
+                Zombies.Count = 0;
+                Zombies.PerTick = 0;
+                Zombies.PerClick = 1;
+            }
         }
         /// <summary>
         /// Class storing the data on the total amount of zombies there are in the game.
@@ -27,55 +57,71 @@ namespace ZombieSim
         public class Zombies
         {
             public int Count { get; set; } //amount of zombies had
+            public int BrainsCount { get; set; } //amount of brains had
             public int PerTick { get; set; } //ingrowth of zombies per second
-            public Zombies(int count, int perTick)
-            { //construtor
-                Count = count;
-                PerTick = perTick;
+            public int PerClick { get; set; } //ingrowth of zombies per click
+            public void addZombies(int number)
+            {
+                Count += number;
+                BrainsCount += number;
             }
         }
+        /// <summary>
+        /// Class storing the data on the particular upgrades unlocked throrough the game
+        /// </summary>
         public class Upgrades
         {
             public string Name { get; set; } //name of the upgrade
             public string Picture { get; set; } //picture of the upgrade
             public string Description { get; set; } //description of the upgrade
+            public float Cost { get; set; } //cost of buying this upgrade
             public int Count { get; set; } //amount of the particular upgrade
-            public int ZombiesPerTick { get; set; } //amount f zombie per second gien by this particular upgrade
-            public Upgrades(string name, string picture, string description, int count, int zombiesPerTick)
-            { //constructor
+            public int ZombiesPerTick { get; set; } //amount of zombie per second given by this particular upgrade
+            public int ZombiesPerClick { get; set; } //amount of zombie per click given by this particular upgrade
+            public Upgrades(string name, string picture, string description, float cost, int count, int zombiesPerTick, int zombiesPerClick)
+            {
                 Name = name;
                 Picture = picture;
                 Description = description;
+                Cost = cost;
                 Count = count;
                 ZombiesPerTick = zombiesPerTick;
+                ZombiesPerClick = zombiesPerClick;
             }
         }
 
+        /// <summary>
+        /// Main form of the game
+        /// </summary>
         public FormMain()
         {
             InitializeComponent();
-            Zombies zombies = new Zombies(0, 1);
-            GameData gameData = new GameData();
-            gameData.Zombies = zombies;
-
-            MessageBox.Show("Every time you press a button, one person somewhere in the world gets turned into a zombie.\n" +
-                "Your goal is to turn the whole humanity into zombies. Using brains (1 zombie = 1 brain), you can unlock unique upgrades which speed up this process.\n\n" +
-                "Good luck!",
-                gameData.Title,
+            MessageBox.Show(GameData.GameDescription,
+                GameData.Title,
                 MessageBoxButtons.OK,
                 MessageBoxIcon.Information);
+            GameData.fillUpgradeList();
+            foreach (Upgrades u in GameData.Upgrades)
+            {
+                listBoxUpgrades.Items.Add(u.Name);
+            }
             timerCount.Start();
         }
         /// <summary>
-        /// Funkcja obsługująca zdarzenie pojedynczego ticku głównego timera gry.
+        /// Function responsible for the main timer of the game.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void timerCount_Tick(object sender, EventArgs e)
         {
+            GameData.Zombies.addZombies(GameData.Zombies.PerTick);
+            labelZombieCount.Text = GameData.Zombies.Count.ToString();
+            labelBrainsCount.Text = GameData.Zombies.BrainsCount.ToString();
+            labelZombiesPerSecond.Text = GameData.Zombies.PerTick.ToString() + " zombies per second";
+            labelZombiesPerClick.Text = GameData.Zombies.PerClick.ToString() + " zombies per click";
         }
         /// <summary>
-        /// Zmienia obrazek przycisku na buttonPushed po wciśnięciu lewego przycisku myszy
+        /// Changes the button's picture to buttonPushed after pressing LMB.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -84,7 +130,7 @@ namespace ZombieSim
             pictureBoxButton.Image = ZombieSim.Properties.Resources.buttonPushed;
         }
         /// <summary>
-        /// Zmienia obrazek przycisku na buttonReady, kiedy przycisk zostaje zwolniony
+        /// Changes the button's picture to buttonReady after pressing LMB.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -99,7 +145,9 @@ namespace ZombieSim
         /// <param name="e"></param>
         private void pictureBoxButton_Click(object sender, EventArgs e)
         {
-
+            GameData.Zombies.addZombies(GameData.Zombies.PerClick);
+            labelZombieCount.Text = GameData.Zombies.Count.ToString();
+            labelBrainsCount.Text = GameData.Zombies.BrainsCount.ToString();
         }
     }
 }
